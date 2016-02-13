eb9274e280192a64f5a23bacea483175f47a075a
GrantColwell <GrantColwell@users.noreply.github.com>
2016-02-13 13:56:49 -0500

    Update Output.java

--- src/ca/_4976/io/Output.java
+++ src/ca/_4976/io/Output.java
@@ -73,8 +73,8 @@ public class Output {
     }
 
     public enum PneumaticSolenoid {
-        Intake(1, 5),
-        HOOD(2, 6);
+        Intake(2, 3);
+        //HOOD(2, 6);
 
         public int port1, port2;
         public DoubleSolenoid solenoid;
